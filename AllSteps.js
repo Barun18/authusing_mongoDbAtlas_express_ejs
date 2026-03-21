b5f70522a302c515b428b1e4c80f014a7720698b
Barun Ghorai <ranghorai865@gmail.com>
2026-03-22 02:57:36 +0530

    Authentication using express + Ejs + mongoDB

---
+++ AllSteps.js
@@ -0,0 +1,22 @@
+//! All steps for creating an authentication system using express + Ejs + mongoose
+//TODO:-- create package.json
+//  npm init -y
+//TODO:-- install all required dependencies
+// npm i express  cookie-parser bcrypt jsonwebtoken ejs
+//TODO:-- create a basic server
+//Importing the express and using this call get() and send son=me response to the port
+//TODO:-- require(because we use coomonjs) all necessary packages
+// cookieParser, path
+//? I have use 'ejs' to render HTML pages  and send the variable value
+// LIKE:-- app.set("view engine","ejs")
+//         res.render("HTML file name",{name:"barun"})
+//? static() indicates to the express to serve static files.
+//? express.static(path.join(__dirname, 'public'))--> serve static files from public folder.
+//! From now Authentication and Authorization is started
+//TODO:-- Create two folder: views( contains all html pages) and public(contains all static pages)
+//? Inside public folder create another 3 folders name: images,stylesheets,javascript
+//? Inside  views folder create index.ejs file 
+
+
+
+
---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", function(req,res){
+    res.render("index");
+})
+
+app.listen(3000);
